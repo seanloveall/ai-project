42ac768b4ab6b977d6f36d50b707d5767338625a
seanloveall <sloveall@stevens.edu>
2015-12-17 00:49:54 -0500

    First Init

---
+++ Main.py
@@ -0,0 +1,48 @@
+import sys
+
+from PIL import Image
+
+if(len(sys.argv) != 5):
+    print "Must provide 4 arguments, first two arguments are input files, third argument is output file, and 4th argument is which alogirthm to use"
+    sys.exit(1)
+
+knownfiles = []
+unknownfiles = []
+
+#gets the known files
+with open(sys.argv[1]) as inputfile:
+    knownfiles = inputfile.read().split('\n')
+
+#gets the unknown files
+with open(sys.argv[2]) as inputfile:
+    unknownfiles = inputfile.read().split('\n')
+
+#split up each line, also remove any lines that are empty
+i = 0
+while i < len(knownfiles):
+    if(knownfiles[i] == ""):
+        knownfiles.pop(i)
+    else:
+        knownfiles[i] = [knownfiles[i][:-2], knownfiles[i][-1:]]
+        i = i + 1
+
+#add a value for each line, also remove any lines that are empty
+i = 0
+while i < len(unknownfiles):
+    if(unknownfiles[i] == ""):
+        unknownfiles.pop(i)
+    else:
+        unknownfiles[i] = [unknownfiles[i], 0]
+        i = i + 1
+
+print knownfiles
+
+print unknownfiles
+
+#magic happens here
+
+#outputs the data
+with open(sys.argv[3], 'w+') as outputfile:
+    for elem in unknownfiles:
+        outputfile.write(" ".join(str(v) for v in elem))
+        outputfile.write("\n")
---
+++ README.txt
@@ -0,0 +1,5 @@
+Written in Python. The first argument is a text file that contains a list of paths to images as well as an indicator of 0 or 1. The second argument is a text file that contains a list of paths to images. The third argument is the path to the output file. The fourth argument is a number that tells the application which algorithm to use.
+
+Example of Command Line Execution
+
+python.exe ai-project\Main.py C:/Python27/ai-project/input.txt C:/Python27/ai-project/input2.txt C:/Python27/ai-project/output.txt 2
