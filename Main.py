52a54a4d3a0e6023c10d62be78826cec284ed308
seanloveall <sloveall@stevens.edu>
2015-12-19 18:32:25 -0500

    More stuff

--- Main.py
+++ Main.py
@@ -1,6 +1,53 @@
 import sys
 
 from PIL import Image
+from PIL import ImageFilter
+
+def iround(num):
+    return int(round(num))
+
+def grayscale_image(img):
+    width, height = img.size
+    
+    for x in range(width):
+        for y in range(height):
+            red, green, blue = img.getpixel((x, y))
+
+            #formula found on the web to convert to grayscale
+            col = iround(0.3 * red + 0.59 * green + 0.11 * blue)
+            
+            img.putpixel((x, y), (col, col, col))
+            
+    return img
+
+def resize_and_crop(img, size, crop_type='middle'):
+    width, height = img.size
+    img_ratio = float(width) / height
+    nwidth, nheight = size
+    ratio = float(nwidth) / nheight
+    
+    
+    #Scales either vertically or horizontally and then crop the image
+    if ratio > img_ratio:
+        #scale vertically
+        img = img.resize((nwidth, iround(nheight * img_ratio)), Image.ANTIALIAS)
+
+        #crop in the middle
+        box = (0, iround((img.size[1] - nheight) / 2), nwidth, iround((img.size[1] + nheight) / 2))
+        img = img.crop(box)
+    elif ratio < img_ratio:
+        #scale horizontally
+        img = img.resize((iround(size[0] * img_ratio), size[1]), Image.ANTIALIAS)
+
+        #crops in the middle
+        box = (iround((img.size[0] - nwidth) / 2), 0, iround((img.size[0] + nwidth) / 2), nheight)
+        img = img.crop(box)
+    else :
+        #scale 1:1
+        img = img.resize((nwidth, nheight), Image.ANTIALIAS)
+        
+    return img
+    
 
 if(len(sys.argv) != 5):
     print "Must provide 4 arguments, first two arguments are input files, third argument is output file, and 4th argument is which alogirthm to use"
@@ -35,11 +82,24 @@ while i < len(unknownfiles):
         unknownfiles[i] = [unknownfiles[i], 0]
         i = i + 1
 
-print knownfiles
-
-print unknownfiles
 
 #magic happens here
+        
+
+#### BEGIN SEAN ####
+i = 0
+avg = [0, 0, 0]
+#while i < len(knownfiles):
+
+image = Image.open(knownfiles[2][0])
+image = grayscale_image(image)
+image = image.filter(ImageFilter.BLUR)
+image = resize_and_crop(image, (100, 100))
+image.save("C:/Python27/test.jpg")
+#### END SEAN ####
+
+
+
 
 #outputs the data
 with open(sys.argv[3], 'w+') as outputfile:
